496edaed43f9265c0f9823b3394000708e16158d
cg <congvc98@gmail.com>
2024-12-09 17:09:19 +0700

    delete/remove accounts logic

---
+++ src/app/(dashboard)/accounts/columns.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { InferResponseType } from "hono";
+import { ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { client } from "@/lib/hono";
+
+import { Actions } from "./actions";
+
+export type ResponseType = InferResponseType<
+  typeof client.api.accounts.$get,
+  200
+>["data"][0];
+
+export const columns: ColumnDef<ResponseType>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+    meta: {
+      headerClassName: "w-16",
+    },
+  },
+  {
+    accessorKey: "name",
+    enableResizing: true,
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="px-0"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    accessorKey: "Actions",
+    meta: {
+      headerClassName: "text-right",
+      rowClassName: "text-right",
+    },
+    cell: ({ row }) => <Actions id={row.original.id} />,
+  },
+];
